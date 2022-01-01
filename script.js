0a3042ec1b52cc64bc88ad285c66455221132db2
barath sam <barathsam555.p@gmail.com>
2022-01-01 12:09:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+async function fetchData(){
+  var response = await fetch("https://api.github.com")
+    return response
+  }
+  fetchData()
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(response){
+    console.log(response)
+  })
+  .catch(function(err){
+    console.log(err)
+  })
+
+
+  async function user(){
+    await fetch("https://api.github.com/users/mojombo").then(function(response){
+      return response.json()
+
+    })
+    .then(function(response){
+      console.log(response)
+    })
+  }
+ user()
+
+  async function repo(){
+    await fetch("https://api.github.com/users/mojombo/repos").then(function(response){
+      return response.json()
+
+    })
+    .then(function(response){
+      console.log(response)
+    })
+  }
+ repo()
+
+
+fetch("https://api.github.com/users/mojombo/repos")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                appendData(data);
+            })
+            .catch(function (err) {
+                console.log('error: ' + err);
+            });
+        function appendData(data) {
+            var mainContainer = document.getElementById("myData");
+                var div = document.createElement("div");
+                div.innerHTML = 'Name: ' + data[0].owner.login+
+                                '<br> Forks Count: ' + data[0].forks_count +
+                                '<br> Star Count:' + data[0].stargazers_count +
+                                '<br> <br> Owner Profile <br>'+      
+                                  '<img src="'+data[0].owner.avatar_url+'"/>';
+                                
+                mainContainer.appendChild(div);
+            
+        }
